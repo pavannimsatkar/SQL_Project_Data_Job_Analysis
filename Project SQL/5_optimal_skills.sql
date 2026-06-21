9ce106783c3d168577e6e32b6c8bcdb94cf6d709
Pavan Nimsatkar <nimsatkarpavan@gmail.com>
2026-06-21 17:09:58 +0530

    written the query more concisely

--- Project SQL/5_optimal_skills.sql
+++ Project SQL/5_optimal_skills.sql
@@ -56,3 +56,25 @@ ORDER BY
 LIMIT 25
 
 
+-- rewriting this same query more concisely
+
+SELECT 
+    skills_dim.skill_id,
+    skills_dim.skills,
+    count(skills_job_dim.job_id) AS demand_count,
+    ROUND(avg(salary_year_avg), 0) as avg_salary
+FROM
+    job_postings_fact
+INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim on skills_job_dim.skill_id = skills_dim.skill_id
+WHERE
+    salary_year_avg IS NOT NULL AND
+    job_work_from_home = 'True' AND
+    job_title_short = 'Data Analyst'
+GROUP BY
+    skills_dim.skill_id
+HAVING
+    count(skills_job_dim.job_id) > 10
+ORDER BY
+    avg_salary DESC
+LIMIT 25;
